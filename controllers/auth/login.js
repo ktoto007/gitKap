df8b51cf069789d2cadf810e168ab25ee4bc4745
ktoto007 <55619028+ktoto007@users.noreply.github.com>
2025-03-04 11:21:33 +0200

    Update login.js

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -14,10 +14,6 @@ const login = async (req, res) => {
     throw HttpError(401, "Email or password is wrong");
   }
 
-  if (!user.verify) {
-    throw HttpError(401, "Email not Verified");
-  }
-
   const passwordCompare = await bcrypt.compare(password, user.password);
 
   if (!passwordCompare) {
